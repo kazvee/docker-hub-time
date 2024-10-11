74ecf9e59f64e80c6dfa8cb313cabfbb1c42028d
kazvee <kazveedev@gmail.com>
2024-10-11 17:24:42 -0600

    Add entrypoint script

---
+++ webserver.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+start_server() {
+  echo "Server started! 😄 Visit http://localhost:5000 to use it! 😎"
+  message=$(echo "<html><body><p>Beep Boop! Today is: $(date).</p></body></html>")
+  length=$(wc -c <<< "$message")
+  payload="\
+HTTP/1.1 200 OK
+Content-Length: $((length-1))
+
+$message"
+  while true
+  do echo -ne "$payload" | nc -l -p 5000
+  done
+}
+
+start_server
